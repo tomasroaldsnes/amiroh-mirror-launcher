a9db05118b9bb0e0f4595bb4c2d935eaa788fe82
androidkhurram <androidkhurram@users.noreply.github.com>
2018-03-12 22:20:54 -0500

    Add files via upload

---
+++ src/main/java/amiroh_mirror_launcher/com/amiroh_mirror_launcher/MainActivity.java
@@ -0,0 +1,13 @@
+package amiroh_mirror_launcher.com.amiroh_mirror_launcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
